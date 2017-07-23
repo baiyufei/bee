2b8fb53f82b0aa8289ee90ef38c5f78c54a804c8
baiyufei <baiyufei@outlook.com>
2017-07-23 17:22:05 +0800

    gtest

--- cpp/s5.cc
+++ cpp/s5.cc
@@ -13,3 +13,10 @@ TEST(FooTest, HandleNoneZeroInput)
     EXPECT_EQ(2, Foo(4, 10));
     EXPECT_EQ(6, Foo(30, 18));
 }
+
+int main(int argc, char **argv)
+{ 
+    testing::InitGoogleTest(&argc,argv); 
+    RUN_ALL_TESTS(); 
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 - [s2](/cpp/s2.cc) 策略模式
 - [s3](/cpp/s3.cc) 观察者模式
 - [s4](/cpp/s4.cc) 实现 map 的 string key 不区分大小写
+- [s5](/cpp/s5.cc) gtest 测试
